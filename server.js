1d3aae73995c39535417f90bb2001fff2b67d8e3
soniyuvraj2006 <soniyuvraj2006@gmail.com>
2026-01-16 00:04:19 +0530

    Update server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
 
